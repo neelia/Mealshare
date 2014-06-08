32637eb5d2af7890d6303ad9fc6c9a28883c3b96
Aileen <aileenvoelker@Aileens-MacBook-Pro.local>
2014-06-08 14:46:04 +0200

    test3

--- GLFtv/header.php
+++ GLFtv/header.php
@@ -48,11 +48,12 @@
 	<link href="<?php bloginfo ('template_directory'); ?>/plugins/pushy-master/css/pushy.css" rel="stylesheet">
 	<link rel="stylesheet" href="//releases.flowplayer.org/5.4.6/skin/minimalist.css">
 	<link rel="stylesheet" type="text/css" media="all" href="<?php bloginfo( 'stylesheet_url' ); ?>" />
-	<link rel="stylesheet" href="<?php bloginfo ('template_directory'); ?>/home.css">
+	<link href="<?php bloginfo ('template_directory'); ?>/home.css" rel="stylesheet">
+	<link href="<?php bloginfo ('template_directory'); ?>/info.css" rel="stylesheet">
 	<script src="<?php bloginfo ('template_directory'); ?>/vendor/modernizr-2.6.2.min.js"></script>
 	<script src="http://ajax.googleapis.com/ajax/libs/jquery/2.0.3/jquery.min.js"></script>
 	<script>
-		window.jQuery || document.write('<script src="vendor/jquery-2.0.3.min.js"><\/script>')
+	window.jQuery || document.write('<script src="vendor/jquery-2.0.3.min.js"><\/script>')
 	</script>
 	<script src="<?php bloginfo ('template_directory'); ?>/plugins/flexslider/jquery.flexslider-min.js" type="text/javascript"></script>
 	<script src="<?php bloginfo ('template_directory'); ?>/plugins/nouislider/jquery.nouislider.min.js" type="text/javascript"></script>
@@ -63,13 +64,13 @@
 	<script type="text/javascript" src="<?php bloginfo ('template_directory'); ?>/plugins/bxslider-4-master/jquery.bxslider.min.js"></script>
 	<script type="text/javascript" src="<?php bloginfo ('template_directory'); ?>/js/custom_ajax.js"></script>
 	<script>
-		(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
-			(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
-			m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-		})(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+	(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+		(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+		m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+	})(window,document,'script','//www.google-analytics.com/analytics.js','ga');
 
-		ga('create', 'UA-45105950-1', 'glftv.de');
-		ga('send', 'pageview');
+	ga('create', 'UA-45105950-1', 'glftv.de');
+	ga('send', 'pageview');
 
 	</script>
 </head>
